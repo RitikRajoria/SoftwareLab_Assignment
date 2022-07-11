73650d6f6660c785b4dcda1a01b9c9542f59c1b6
RitikRajoria <ritikrajrrr@gmail.com>
2022-07-11 17:29:14 +0530

    signup page ui done

--- lib/pages/SignUp/signup.dart
+++ lib/pages/SignUp/signup.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../../widgets/widgetsUi.dart';
+
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
@@ -10,20 +12,145 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController fullName = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController phoneNumber = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  TextEditingController rePass = TextEditingController();
 
-  final _formKey = GlobalKey<FormState>();  
-  
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(child: SingleChildScrollView(child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: Column(
+                children: [
+                  // first heading
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'FarmerEats',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+
+                  //sign up page no. text
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Signup 1 of 4',
+                      style:
+                          TextStyle(fontSize: 14, color: Colors.grey.shade400),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+
+                  //welcome text
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Welcome!',
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+
+                  //icon buttons only ui
+                  iconButtons(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  //Text
+                  Text(
+                    'or signup with',
+                    style: TextStyle(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  //email & password field
+                  Container(
+                    height: 400,
+                    child: Column(
+                      children: [
+                        //full name
+                        inputFields('assets/Group 54@3x.png', 'Full Name',
+                            false, fullName, false),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        //email
+                        inputFields('assets/Vector@3x-2.png', 'Email Address',
+                            false, fullName, false),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        //phone number
+                        inputFields('assets/Vector@3x-1.png', 'Email Address',
+                            false, phoneNumber, false),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        //password field
+                        inputFields('assets/Group 47@3x.png', 'Enter Password',
+                            true, pass, true),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        inputFields('assets/Group 47@3x.png',
+                            'Re-enter Password', true, rePass, true),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  //sign up text
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //login button
+                      InkWell(
+                        onTap: () {},
+                        child: buttonWidgetUnderlined('Login'),
+                      ),
+
+                      //continue button
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: (size.width) * 0.58,
+                          child: buttonWidget(
+                            'Continue',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),),),
+      ),
     );
   }
 }
