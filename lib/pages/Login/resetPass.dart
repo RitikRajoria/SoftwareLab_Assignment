4eb512148c7fb5877375dc33201eb13b37904e80
RitikRajoria <ritikrajrrr@gmail.com>
2022-07-11 16:47:11 +0530

    Reset Password ui done

--- lib/pages/Login/resetPass.dart
+++ lib/pages/Login/resetPass.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/widgetsUi.dart';
 
 class ResetPass extends StatefulWidget {
   const ResetPass({Key? key}) : super(key: key);
@@ -9,20 +10,109 @@ class ResetPass extends StatefulWidget {
 }
 
 class _ResetPassState extends State<ResetPass> {
+  TextEditingController newPass = TextEditingController();
+  TextEditingController reEnterPass = TextEditingController();
 
-  final _formKey = GlobalKey<FormState>();  
-  
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: SingleChildScrollView(child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  // first heading
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'FarmerEats',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+
+                  //Forgot pass text
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Reset Password',
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+
+                  //login again text
+                  Row(
+                    children: [
+                      Text(
+                        'Remember your password?',
+                        style: TextStyle(color: Colors.grey.shade500),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Color(0xffd5715b),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+
+                  //password fields
+                  Container(
+                    height: 200,
+                    child: Column(
+                      children: [
+                        //new password
+                        inputFields('assets/Group 47@3x.png', 'NewPassword',
+                            false, newPass, false),
+
+                        const SizedBox(
+                          height: 28,
+                        ),
+                        //re-enter password
+                        inputFields('assets/Group 47@3x.png',
+                            'Confirm New Password', false, reEnterPass, false),
+
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  //submit button
+                  InkWell(
+                    onTap: () {},
+                    child: buttonWidget(
+                      'Submit',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 34,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),),),
+      ),
     );
   }
 }
