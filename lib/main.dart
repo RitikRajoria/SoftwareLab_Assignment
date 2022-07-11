290ae25d4545a760f0a65da9a0d68f8c5a669b16
RitikRajoria <ritikrajrrr@gmail.com>
2022-07-11 15:49:47 +0530

    theme for input fields

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:softwarelab_assignment/pages/Login/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              width: 2,
+              color: Colors.transparent,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              width: 2,
+              color: Color(0xffd5715b),
+            ),
+          ),
+        ),
+      ),
       home: LoginPage(),
     );
   }
