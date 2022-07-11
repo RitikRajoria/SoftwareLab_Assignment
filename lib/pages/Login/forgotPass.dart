67f1c989a7c36eda2fa5ac1fa89335f7f5775ac4
RitikRajoria <ritikrajrrr@gmail.com>
2022-07-11 16:14:44 +0530

    forgot password ui done

--- lib/pages/Login/forgotPass.dart
+++ lib/pages/Login/forgotPass.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:softwarelab_assignment/widgets/widgetsUi.dart';
 
 class ForgotPass extends StatefulWidget {
   const ForgotPass({Key? key}) : super(key: key);
@@ -9,20 +9,99 @@ class ForgotPass extends StatefulWidget {
 }
 
 class _ForgotPassState extends State<ForgotPass> {
+  TextEditingController phoneController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
-  final _formKey = GlobalKey<FormState>();  
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: SingleChildScrollView(child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  // first heading
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'FarmerEats',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+
+                  //Forgot pass text
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Forgot Password?',
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+
+                  //login again text
+                  Row(
+                    children: [
+                      Text(
+                        'Remember your password?',
+                        style: TextStyle(color: Colors.grey.shade500),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Color(0xffd5715b),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+
+                  //phone no. field
+                  Container(
+                    height: 100,
+                    child: Column(
+                      children: [
+                        //email
+                        inputFields('assets/Vector@2x-1.png', 'Phone Number',
+                            false, phoneController, false),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  //login button
+                  InkWell(
+                    onTap: () {},
+                    child: buttonWidget(
+                      'Send Code',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 34,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),),),
+      ),
     );
   }
 }
