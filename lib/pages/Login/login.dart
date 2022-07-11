6e1843defe109b8ac8ff2351067c6469fe022a52
RitikRajoria <ritikrajrrr@gmail.com>
2022-07-11 16:00:16 +0530

    login page ui complete

--- lib/pages/Login/login.dart
+++ lib/pages/Login/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/widgetsUi.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,20 +10,101 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
-  final _formKey = GlobalKey<FormState>();  
-  
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: SafeArea(child: SingleChildScrollView(child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'FarmerEats',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Welcome back!',
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'New Here?',
+                        style: TextStyle(color: Colors.grey.shade500),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Create account',
+                          style: TextStyle(
+                            color: Color(0xffd5715b),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    height: 180,
+                    child: Column(
+                      children: [
+                        inputFields('assets/Vector@3x-2.png', 'Email Address',
+                            false, emailController, false),
+                        const SizedBox(
+                          height: 28,
+                        ),
+                        inputFields('assets/Group 47@3x.png', 'Password', true,
+                            passwordController, true),
+                      ],
+                    ),
+                  ),
+                  buttonWidget(
+                    'Login',
+                  ),
+                  const SizedBox(
+                    height: 34,
+                  ),
+                  Text(
+                    'or login with',
+                    style: TextStyle(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  iconButtons(),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),),),
+      ),
     );
   }
 }
